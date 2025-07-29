80110914dcdbae31507515d8585ece26cc65c9d2
WyiZhng <491222058@qq.com>
2025-07-29 10:16:57 +0800

    2.1版本前端

--- util.ts
+++ util.ts
@@ -4,4 +4,4 @@ import {type VueCookies} from "vue-cookies";
 export async function updateUserInfo($cookies:VueCookies,store:{detail:any}){
     let resp = await getUserInfo($cookies.get('token'))
     store.detail = resp.data.form;
-}
+}//123
